2b0406163c49041252c479300460c21fec020c18
mmmacedo <murilommacedo666@gmail.com>
2021-09-08 20:03:11 -0300

    Projetos iniciados

---
+++ backend/src/test/java/com/br/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
