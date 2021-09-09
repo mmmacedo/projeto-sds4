2974b9a7ba0779bc03579909660d735e23a356c6
mmmacedo <murilommacedo666@gmail.com>
2021-09-08 21:14:21 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import DataTable from "./components/DataTable/DataTable";
+
 function App() {
-  return (
-    <div>
-      <h1 className="text-primary">Olá mundão</h1>
-    </div>
-  );
+    return (
+        <>
+            <NavBar></NavBar>
+            <div className="container">
+                <h1 className="text-primary">Olá mundão</h1>
+                <DataTable></DataTable>
+            </div>
+            <Footer></Footer>
+        </>
+    );
 }
 
 export default App;
