44e07ac278223588f3acf299e118d655d469e71b
mmmacedo <murilommacedo666@gmail.com>
2021-09-11 16:58:26 -0300

    Pagination

--- frontend/src/components/DataTable/DataTable.tsx
+++ frontend/src/components/DataTable/DataTable.tsx
@@ -3,8 +3,10 @@ import {SalePage} from "../../types/sale";
 import axios from "axios";
 import {BASE_URL} from "../../utils/request";
 import {formatLocalDate} from "../../utils/format";
+import Pagination from "../Pagination";
 
 const DataTable = () => {
+
     const [page, setPage] = useState<SalePage>({
         first: true,
         last: true,
@@ -13,40 +15,50 @@ const DataTable = () => {
         totalPages: 0
     });
 
+    const [activePage, setActivePage] = useState<number>(0);
+
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales?page=1&size=10&sort=date,desc`)
+        axios.get(`${BASE_URL}/sales?page=${activePage}&size=10&sort=date,desc`)
             .then(response => {
                 setPage(response.data);
             })
-    }, [])
+    }, [activePage])
+
+    const changePage = (index: number): void => {
+        setActivePage(index);
+    }
 
     return (
-        <div className="table-responsive">
-            <table className="table table-striped table-sm">
-                <thead>
-                <tr>
-                    <th>Data</th>
-                    <th>Vendedor</th>
-                    <th>Clientes visitados</th>
-                    <th>Negócios fechados</th>
-                    <th>Valor</th>
-                </tr>
-                </thead>
-                <tbody>
-                {
-                    page.content?.map(item => (
-                        <tr key={item.seller.name}>
-                            <td>{formatLocalDate(item.date, 'dd/MM/yyyy')}</td>
-                            <td>{item.seller.name}</td>
-                            <td>{item.visited}</td>
-                            <td>{item.deals}</td>
-                            <td>{item.amount.toFixed(2)}</td>
-                        </tr>
-                    ))
-                }
-                </tbody>
-            </table>
-        </div>
+        <>
+            <Pagination page={page} onPageChange={changePage}/>
+
+            <div className="table-responsive">
+                <table className="table table-striped table-sm">
+                    <thead>
+                    <tr>
+                        <th>Data</th>
+                        <th>Vendedor</th>
+                        <th>Clientes visitados</th>
+                        <th>Negócios fechados</th>
+                        <th>Valor</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        page.content?.map(item => (
+                            <tr key={`${item.seller.name}${item.date}`}>
+                                <td>{formatLocalDate(item.date, 'dd/MM/yyyy')}</td>
+                                <td>{item.seller.name}</td>
+                                <td>{item.visited}</td>
+                                <td>{item.deals}</td>
+                                <td>{item.amount.toFixed(2)}</td>
+                            </tr>
+                        ))
+                    }
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
 
--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -1,3 +1,3 @@
-//export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = 'http://localhost:8080';
 
-export const BASE_URL = 'https://sds-4-mmmacedo.herokuapp.com';
+//export const BASE_URL = 'https://sds-4-mmmacedo.herokuapp.com';
