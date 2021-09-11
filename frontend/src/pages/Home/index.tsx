de5cb5b42d9f49dd357d637d771a39a44cd7bc47
mmmacedo <murilommacedo666@gmail.com>
2021-09-11 14:27:11 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,22 @@
+import Footer from "../../components/Footer/Footer";
+import NavBar from "../../components/NavBar/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <h1>home</h1>
+        <>
+            <NavBar/>
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4"> DSVendas</h1>
+                    <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                    <hr/>
+                    <p>Esta aplicação consiste em exibir um dashboard.</p>
+                    <Link className="btn btn-primary btn-lg" to="/dashboard">Acessar o dashboard</Link>
+                </div>
+            </div>
+            <Footer/>
+        </>
     );
 }
 
